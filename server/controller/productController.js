b746e928d91474f79ce60da19bce1a72f663b073
vivekkumar001 <vivekkumar981881@gmail.com>
2026-01-14 17:02:40 +0530

    update

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -207,3 +207,68 @@ export const updateProductController = async (req, res) => {
     });
   }
 };
+
+// FILTER PRODUCTS
+export const productFilterController = async (req, res) => {
+  try {
+    const { checked, radio } = req.body;
+    let args = {};
+    if (checked.length > 0) args.category = checked;
+    if (radio.length) args.price = { $gte: radio[0], $lte: radio[1] };
+    const products = await productModel.find(args);
+    res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error While Filtering Products",
+      error,
+    });
+  }
+};
+
+// PRODUCT COUNT
+export const productCountController = async (req, res) => {
+  try {
+    const total = await productModel.find({}).estimatedDocumentCount();
+    res.status(200).send({
+      success: true,
+      total,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error in product count",
+      error,
+    });
+  } 
+};
+
+// PRODUCT LIST PER PAGE
+export const productListController = async (req, res) => {
+  try {
+    const perPage = 6;
+    const page = req.params.page ? req.params.page : 1;
+    const products = await productModel
+      .find({})
+      .select("-photo")
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .sort({ createdAt: -1 });
+    res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error in per page ctrl",
+      error,
+    });
+  }
+};
